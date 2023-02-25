d82dc4456a583929e80667e47406b5800ce720de
Sumit Kumar <sk4390789@gmail.com>
2023-02-25 09:53:30 +0530

    Scheduling Algo FCFS

---
+++ Os/lib/Process.js
@@ -0,0 +1,10 @@
+class Process {
+    constructor(id, arrivingTime, burstTime) {
+        this.id = this.id;
+        this.arrivingTime = this.arrivingTime;
+        this.burstTime = this.burstTime;
+        this.completionTime;
+        this.turnAroundTime;
+        this.watingTime;
+    }
+}
